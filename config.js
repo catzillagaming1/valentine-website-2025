5b0995648e0dd1685375aef5ce69850065c13ff9
catzillagaming1 <catzillagaming1@gmail.com>
2026-02-13 23:36:55 +0400

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://audio.com/indra-path/audio/anlatamam, // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dchpewetr/video/upload/v1771011217/SPOTDOWNLOADER.COM_Anlatamam_caqzeb.mp3, // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
